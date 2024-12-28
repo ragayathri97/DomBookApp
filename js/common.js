9e0da664c88e487112f484d8c29f4e70007d52d4
ragayathri97 <ragayathri97@gmail.com>
2024-12-28 15:56:50 +0530

    completed books.js

--- js/common.js
+++ js/common.js
@@ -6,6 +6,11 @@ async function fetchData(url){
         if(!response.ok){
             throw new Error('HTTP erro! status: ${response.status}');
         }
+        if(!response.ok){
+            throw new Error(`HTTP error status:${response.status}`);
+        }
+        return await response.json();
+    }
         catch(error) {
             displayError('Failed to fetch data: ${error.message}');
             throw error;
@@ -18,4 +23,3 @@ async function fetchData(url){
         errorDiv.textContent=message;
         document.getElementById('app').appendChild(errorDiv)
     }
-}
